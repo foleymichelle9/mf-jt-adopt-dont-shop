420a56fe49f3e1117c2e84df6410ff9ec37e33c1
Josh Tukman <josh.tukman@gmail.com>
2020-05-13 20:38:06 -0600

    Pet can only be favorited one time

--- spec/features/favorites/add_pet_spec.rb
+++ spec/features/favorites/add_pet_spec.rb
@@ -1,26 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe 'favorite indicator in nav bar' do
-  it 'displays the total number of pets favorited in the nav bar' do
-    shelter1 = Shelter.create(name: "Pet House",
+  before(:each) do
+    @shelter1 = Shelter.create(name: "Pet House",
                               address: "12 Main St.",
                               city: "Denver",
                               state: "Colorado",
                               zip: "80001")
 
-    lucille = shelter1.pets.create!(image: 'https://justsomething.co/wp-content/uploads/2014/08/pitbull-photos-13.jpg',
+    @lucille = @shelter1.pets.create!(image: 'https://justsomething.co/wp-content/uploads/2014/08/pitbull-photos-13.jpg',
                                     name: "Lucille",
                                     age: 3,
                                     status: false,
                                     description: "Lovable pitbull",
                                     sex: "female")
+  end
 
-    visit "/pets/#{lucille.id}"
+  it 'displays the total number of pets favorited in the nav bar' do
+
+    visit "/pets/#{@lucille.id}"
+    expect(page).to have_content("Number of pets favorited: 0")
+
+    click_button "Add to Favorites"
+
+    expect(page).to have_content("Number of pets favorited: 1")
+  end
+
+  it 'I can only favorite a pet once' do
+
+    visit "/pets/#{@lucille.id}"
     expect(page).to have_content("Number of pets favorited: 0")
 
     click_button "Add to Favorites"
 
     expect(page).to have_content("Number of pets favorited: 1")
 
+    click_button "Add to Favorites"
+
+    expect(page).to have_content("You have already favorited #{@lucille.name}")
+    expect(page).to have_content("Number of pets favorited: 1")
   end
 end
